56b3d31250dc974462de3da9af87e75b2c10d2ab
mishima <tsuwtsur@gmail.com>
2026-07-16 14:34:51 +0900

    問題文の仕様を変更

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ApplicationRecord
   belongs_to :genre
   has_many :games
+
+  validates :genre_id, presence: true
+  validates :body, presence: true
+  validates :correct_answer, presence: true
+  validates :wrong_answer_1, presence: true
+  validates :wrong_answer_2, presence: true
+  validates :wrong_answer_3, presence: true
 end
