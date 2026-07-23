293970ff2a4133e26a24b637abef0c152426cd98
yano <r7_yano@icc.core.ac.jp>
2026-07-23 11:00:30 +0900

    quiz-yano7

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@ class GamesController < ApplicationController
     @genres = Genre.all
   end
 
+  # スタートボタン
   def start
     session[:answers] = {}
     genre_id = params[:genre_id]
@@ -73,19 +74,31 @@ class GamesController < ApplicationController
     question = Question.find_by(id: question_id)
 
     unless question
+      reset_quiz_session
       redirect_to root_path, alert: "問題データを取得できませんでした"
       return
     end
 
-    if params[:user_answer_index].blank?
-      redirect_to game_quiz_path, alert: "回答を選択してください"
-      return
-    end
-
-    selected_index = params[:user_answer_index].to_i
+    selected_index = params[:user_answer_index]&.to_i
 
     session[:answers] ||= {}
     session[:answers][question.id.to_s] = selected_index
+
+    order = session[:choice_orders][question.id.to_s]
+
+    original_choices = [
+      question.correct_answer,
+      question.wrong_answer_1,
+      question.wrong_answer_2,
+      question.wrong_answer_3
+    ]
+
+    displayed_choices = order.map { |index| original_choices[index] }
+    user_answer = displayed_choices[selected_index]
+
+    if user_answer == question.correct_answer
+      session[:correct_count] += 1
+    end
 
     session[:question_index] += 1
 
@@ -95,6 +108,7 @@ class GamesController < ApplicationController
       redirect_to game_quiz_path
     end
   end
+
 
   # 前へボタン
   def back
@@ -107,8 +121,7 @@ class GamesController < ApplicationController
   end
 
 
-
-
+  #リザルト画面
   def result
     question_ids = session[:question_ids] || []
     answers = session[:answers] || {}
@@ -150,7 +163,6 @@ class GamesController < ApplicationController
     else
       @comment = "もう一度挑戦してみましょう"
     end
-
   end
 end
 
